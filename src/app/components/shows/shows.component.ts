1190874d6e3c3dfce3962a6a4c06a2636f0b9bd1
unknown <hollahell@gmail.com>
2019-01-30 02:19:03 +0500

    shows api completed

--- src/app/components/shows/shows.component.ts
+++ src/app/components/shows/shows.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {ShowsService} from '../../services/shows.service';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-shows',
@@ -6,10 +9,57 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shows.component.css']
 })
 export class ShowsComponent implements OnInit {
+  public showsData;
+  public clickedShow;
+  public videoId;
+  public closeResult;
 
-  constructor() { }
+  constructor(private shows:ShowsService,
+    private modalService: NgbModal,
+    public sanitizer: DomSanitizer
+    ) { }
 
   ngOnInit() {
+    this.getShows();
+  }
+
+  async getShows(){
+    await this.shows.getShows().subscribe(data => {
+      this.showsData = data
+      console.log(this.showsData.results);
+    }); 
+  }
+
+  async showClicked(event,id){
+    console.log('clicked:',id);
+    await this.shows.getShow(id).subscribe(data => {
+        this.clickedShow = data;
+        console.log('clicked show data:',data);
+        this.clickedShow.videos.results.forEach(({site,key,type}) => {
+            if(site === 'YouTube' && type === 'Trailer' )
+                this.videoId = key;
+                // 'https://www.youtube.com/embed/'+
+            // console.log(this.videoId);
+        });
+    })
+  }
+
+  open(content) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
   }
 
 }
--- src/app/services/shows.service.ts
+++ src/app/services/shows.service.ts
@@ -1,9 +1,46 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {environment} from '../../environments/environment';
+import {Observable} from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShowsService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getShows() : Observable<response> {
+    return this.http.get<response>(`https://api.themoviedb.org/3/discover/tv?api_key=${environment.apiKey}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`);
+  }
+
+  getShow(id) : Observable<show> {
+    return this.http.get<show>(`http://api.themoviedb.org/3/tv/${id}?api_key=${environment.apiKey}&append_to_response=videos`);
+  }
 }
+
+interface response {
+  page : number,
+  results : [show],
+  total_results : number,
+  total_pages : number
+}
+
+interface show {
+  poster_path : string,
+  overview : string,
+  genre_ids : [number],
+  id : number,
+  original_language : string,
+  name : string,
+  original_name : string,
+  backdrop_path : string,
+  popularity : number,
+  vote_count : number,
+  video : boolean,
+  vote_average : number,
+  first_air_date : string,
+  origin_country : [string],
+}
+
